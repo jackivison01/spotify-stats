2993bba6209ea00a80fadc6dfa2c57d8a139ec7e
jackivison01 <jivison2001@gmail.com>
2025-03-29 14:42:48 +0000

    Adding sptofy user profile model for when it is pulled from API

---
+++ main/src/types/spotify.ts
@@ -0,0 +1,36 @@
+// src/types/spotify.ts
+
+export type ExplicitContent = {
+    filter_enabled: boolean;
+    filter_locked: boolean;
+};
+
+export type ExternalUrls = {
+    spotify: string;
+};
+
+export type Followers = {
+    href: string | null;
+    total: number;
+};
+
+export type Image = {
+    url: string;
+    height: number;
+    width: number;
+};
+
+export type SpotifyProfile = {
+    country: string;
+    display_name: string;
+    email: string;
+    explicit_content: ExplicitContent;
+    external_urls: ExternalUrls;
+    followers: Followers;
+    href: string;
+    id: string;
+    images: Image[];
+    product: string;
+    type: string;
+    uri: string;
+};
